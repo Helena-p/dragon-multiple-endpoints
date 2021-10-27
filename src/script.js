708db4f130600d675ab3d96112c163bb01f7b1d6
Helena <helenasleterkesto@gmail.com>
2021-10-27 18:20:07 +0200

    Refactor and changed styling

--- src/script.js
+++ src/script.js
@@ -1,76 +1,61 @@
-const app = document.querySelector("#app");
+const app = document.querySelector(".app");
 const endpointArticles = "https://vanillajsacademy.com/api/dragons.json";
-const endpointAuthors = "https://vanillajsacademy.com/api/dragons-authors.json";
+const endpointBio = "https://vanillajsacademy.com/api/dragons-authors.json";
 
-/**
- * Sanitize and encode all HTML in a user-submitted string
- * https://portswigger.net/web-security/cross-site-scripting/preventing
- * @param  {String} str  The user-submitted string
- * @return {String} str  The sanitized string
- */
-const sanitizeHTML = (str) => {
-    return str
-        .replace(/javascript:/gi, "")
-        .replace(/[^\w-_. ]/gi, function (c) {
-            return `&#${c.charCodeAt(0)};`;
-        });
-};
-
-const getAuthor = (name, authors) => {
-    return authors.find((author) => author.author === name);
-};
-
-const displayData = (articles, authors) => {
-    if (!articles || articles.length < 1) {
-        throw new Error(error);
-    }
-
-    app.innerHTML = articles
-        .map((article) => {
-            let author = getAuthor(article.author, authors);
-            return ` 
-                <div class="article-container">
-                <div class="article-container__box">
-                <h2 class="header-secondary"><a href="${sanitizeHTML(
-                    article.url
-                )}">${sanitizeHTML(article.title)}</a></h2>
-                <h3 class="header-tertiary">By ${
-                    author
-                        ? `${sanitizeHTML(author.author)} - ${sanitizeHTML(
-                              author.bio
-                          )}`
-                        : sanitizeHTML(article.author)
-                }</h3>
-                <p class="paragraph">${sanitizeHTML(article.article)}</p>
-                <p class="paragraph"><small>${sanitizeHTML(
-                    article.pubdate
-                )}</small></p>
-                </div>     
-                </div>`;
+const responseHandler = (responses) => {
+    return Promise.all(
+        responses.map((response) => {
+            if (!response.ok) throw new Error(response.status);
+            return response.json();
         })
-        .join(" ");
+    );
 };
+
+function findAuthor(person, authors) {
+    return authors.find((author) => author.author === person);
+}
 
 const errorHandler = (error) => {
-    console.error(error);
-    app.innerHTML = `
-        <p class='error-message'>No articles found at this point. 
-        Please refresh page or try again later.</p>
-        `;
+    app.textContent = `${error}, No articles available at this time`;
 };
 
-const getArticles = () => {
-    Promise.all([fetch(endpointArticles), fetch(endpointAuthors)])
-        .then((responses) =>
-            Promise.all(
-                responses.map((response) => {
-                    if (!response.ok) throw new Error(error);
-                    return response.json();
-                })
-            )
-        )
-        .then((data) => displayData(data[0].articles, data[1].authors))
-        .catch((error) => errorHandler(error));
+const renderArticles = (data) => {
+    let { articles } = { ...data[0] };
+    let { authors } = { ...data[1] };
+
+    if (!articles || !articles.length) errorHandler();
+
+    app.innerHTML = `
+  ${articles
+      .map((article) => {
+          let bio = findAuthor(article.author, authors).bio;
+
+          return `
+    <h2 class="spacer">
+      <a href=${article.url}>${article.title}</a>
+    </h2>
+    <p class="author">By ${article.author}</p>   
+    <p class="bio">${bio}</p>
+    <p>${article.article}</p>
+    <small>${article.pubdate}</small>`;
+      })
+      .join("")}
+  `;
+};
+
+const getArticles = async () => {
+    try {
+        const responses = await Promise.all([
+            fetch(endpointArticles),
+            fetch(endpointBio),
+        ]);
+        const data = await responseHandler(responses);
+        const renderData = renderArticles(data);
+        return renderData;
+    } catch (error) {
+        errorHandler(error);
+        console.error(error);
+    }
 };
 
 getArticles();
